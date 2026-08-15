0b6e7de85175ad5e731bde146b8f785fe75203c7
kk231323 <15944265+kk231323@user.noreply.gitee.com>
2026-08-16 03:04:47 +0800

    预防缓存穿透，增加功能：查询不存在值后返回前在缓存中插入null值

--- hm-dianping/src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -16,8 +16,7 @@ import javax.annotation.Resource;
 
 import java.util.concurrent.TimeUnit;
 
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TTL;
+import static com.hmdp.utils.RedisConstants.*;
 
 /**
  * <p>
@@ -41,13 +40,21 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         //2.判断是否存在
         if (StrUtil.isNotBlank(shopJson)){
             //3.存在，直接返回
+
             Shop shop= JSONUtil.toBean(shopJson, Shop.class);
             return Result.ok(shop);
+        }
+        //判断命中的是否是空值
+        if(shopJson!=null){
+            //返回错误信息
+            return Result.fail("店铺不存在");
         }
         //4.不存在，查询数据库
         Shop shop = getById(id);
         //5.不存在，返回错误
         if (shop==null){
+            //写入空值到redis，防止缓存穿透
+            stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY+id,"",CACHE_NULL_TTL, TimeUnit.MINUTES);
             return Result.fail("店铺不存在");
         }
         //6.存在，写入redis缓存
